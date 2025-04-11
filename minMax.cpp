411cc987a91d83097aea67fbd2067cef7ec9ae4c
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-04-11 14:50:27 +0530

    I am making the auto_formatting on the minMax.cpp to see if it changes. I am using clanged and nvim. lets see if it works
    this is just an unimportant change so it can be skipped.

--- minMax.cpp
+++ minMax.cpp
@@ -2,27 +2,26 @@
 #include <vector>
 using namespace std;
 
-vector<int> minMax(vector<int>& nums){
-    vector<int> result;
-    int min, max;
-    min = max = nums[0];
-    for(int i = 1; i < nums.size(); i++){
-        if(nums[i] > max) max = nums[i];
-        if(nums[i] < min) min = nums[i];
-    }
-    result.push_back(min);
-    result.push_back(max);
+vector<int> minMax(vector<int> &nums) {
+  vector<int> result;
+  int min, max;
+  min = max = nums[0];
+  for (int i = 1; i < nums.size(); i++) {
+    if (nums[i] > max)
+      max = nums[i];
+    if (nums[i] < min)
+      min = nums[i];
+  }
+  result.push_back(min);
+  result.push_back(max);
 
-    return result;
+  return result;
 }
 
-int main(){
-    vector<int> nums = {1, 3, 4, 5, 7, 29};
-    vector<int> result = minMax(nums);
-    for(auto number : result){
-        cout<< number <<" ";
-
-    }
-
-    
+int main() {
+  vector<int> nums = {1, 3, 4, 5, 7, 29};
+  vector<int> result = minMax(nums);
+  for (auto number : result) {
+    cout << number << " ";
+  }
 }
