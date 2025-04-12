58ff2b7b1993da8aa2055271952947e98139b4c0
Tuhin Mandal <73009536+MandalTuhin@users.noreply.github.com>
2025-04-12 19:43:08 +0530

    add files of neetcode.io and a few patterns from strivers sheet.

---
+++ containsDuplicate.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+bool containsDuplicate(vector<int> &nums) {
+  unordered_set<int> contains;
+  for (int i = 0; i < nums.size(); i++) {
+    if (contains.find(nums[i]) != contains.end()) {
+      return true;
+    }
+    contains.insert(nums[i]);
+  }
+  return false;
+}
+
+int main() {
+  vector<int> nums = {1, 2, 3, 4};
+  containsDuplicate(nums) ? cout << "yes" : cout << "no";
+}
--- pattern6.cpp
+++ pattern6.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 using namespace std;
 
-void nNumberTriangle(int n) {
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < n - i; j++) {
-      cout << j + 1 << " ";
+// void nNumberTriangle(int n) {
+//   for (int i = 0; i < n; i++) {
+//     for (int j = 0; j < n - i; j++) {
+//       cout << j + 1 << " ";
+//     }
+//     cout << "\n";
+//   }
+// }
+
+void printTraingle(int n){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n + i; j++){
+      if(j >= n - i - 1) {
+        cout<< "*";
+      } else {
+        cout<< " ";
+      }
+        
     }
-    cout << "\n";
+    cout<<"\n";
   }
 }
 
 int main() {
-  nNumberTriangle(5);
+  int n = 5;
+  printTraingle(5);
   return 0;
 }
